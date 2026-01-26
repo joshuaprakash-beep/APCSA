ea29274555006e9bf0e7bd02e16944c0454cd5c0
Michael Guo <104408615+powerlock@users.noreply.github.com>
2026-01-26 08:47:15 -0800

    Remove date from NumberJumble.java header

---
+++ Homework/NumberJumble.java
@@ -0,0 +1,78 @@
+/**
+ * NumberJumble.java  
+ *
+ * @author - Jane Doe
+ * @author - Period n
+ * @author - Id nnnnnnn
+ *
+ * @author - I received help from ...
+ *
+ */
+import java.util.ArrayList;
+
+public class NumberJumble
+{
+
+	public static void main(String[] args)
+	{
+		
+
+
+
+		// 1. Create empty ArrayList and name it list
+      
+		//print("1. Empty list", list);
+
+
+		// 2. Fill list with 11 Integer objects which have the values 0 to 10
+
+		//print("2. Filled list", list);
+
+
+		// 3. Rotate list Left.  The first element must be placed at the end.
+		//    Do NOT use a loop!
+    
+
+//		print("3. Rotated left list", list);
+
+
+		// 4. Rotate list Right.  The last element must be placed at the front.
+		//    Do NOT use a loop!
+
+//		print("4. Rotated right list", list);
+
+
+		// 5. Reverse the elements in list.  Use a loop!
+
+//		print("5. Reversed list", list);
+
+
+		// 6. "Double" each element in the list by inserting a second
+		//    reference to each object immediately after the original.
+		//    Use a loop!
+
+//		print("6. Doubled up list", list);
+
+
+		// 7. Replace each of the elements of list with new Integer objects.
+		//    Each new object has a value that is one-third the original value.
+		//    Use a loop!
+
+//		print("7. Divided by 3 list", list);
+
+
+		// 8. Remove all consecutive duplicate elements.  Use a loop!
+
+//		print("8. No consecutive duplicates list", list);
+	}
+
+
+
+	private static void print(String heading, ArrayList<Integer> list)
+	{
+		System.out.println(heading);
+		System.out.println("Size: " + list.size());
+		System.out.println("List: " + list.toString());
+		System.out.println();
+	}
+}
